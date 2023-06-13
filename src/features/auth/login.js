41d79888aa6ad018b7f6972389968a9b06701c28
malla sagar <mallasagar0101@gmail.com>
2023-06-13 15:08:33 +0545

    validated login form

--- src/features/auth/login.js
+++ src/features/auth/login.js
@@ -1,34 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import'./login.css'
 
 
 
 
 
-
 function Mylogin() {
+    
+    let [useremail, setuseremail]=useState()
+    let [userpassword, setuserpassword]=useState()
+    let [emailerror, setemailerror]= useState('')
+    let [passworderror, setpassworderror ]=  useState('')
+
+
+    useEffect(()=>{
+        if(!useremail){
+            setemailerror("Enter Email")
+        }else {
+            setemailerror("")
+        }
+    },[useremail])
+
+    useEffect(()=>{
+        if(!userpassword){
+            setpassworderror("Enter password")
+        }else {
+            setpassworderror(" ")
+        }
+    },[userpassword])
+
+
+    const Handlesubmit=(ev)=>{
+        ev.preventDefault();
+        console.log("useremail:", useremail);
+        console.log("userpassword:", userpassword)
+    }
+
+
+
   return (
    <>
-        <div className='container col-10 col-sm-8 col-md-4  justify-content-center align-item-center mt-5 py-5 px-5 rounded-4  text-white' style={{
+        <form onSubmit={Handlesubmit} className='container col-10 col-sm-8 col-md-4 py-sm-3  justify-content-center align-item-center mt-5 py-5 px-5 rounded-4  text-white' style={{
             backgroundColor:"#735366"
         }}>
             <div className=" col-12 py-2 myloginemail  mt-5">
                 <span className='col-12 justify-content-center'>Enter User Email:</span>
-                <input  className='col-12  rounded-2' type='email'  name="email"></input>
-                <span  className='text-danger'>error</span>
+                <input  className='col-12  rounded-2' type='email'  name="useremail" onChange={(ev)=>{setuseremail(ev.target.value)}}></input>
+                <span  className='text-danger'>{emailerror}</span>
             </div>
             <div className="col-12 ">
                 <span className='col-12'>Enter pasword</span>
-                <input className='col-12  rounded-2' type='password'  name="password"></input>
-                <span className=' text-danger'>error</span>
+                <input className='col-12  rounded-2' type='password'  name="userpassword" onChange={(ev)=>{setuserpassword(ev.target.value)}}></input>
+                <span className=' text-danger'>{passworderror}</span>
             </div>
             <div className='row  col-12  justify-content-center '>
-                <button className='text-center  col-3 col-sm-8 col-md-6 rounded-2  text-light ' style={{
-                    backgroundColor:"var(--darkpurple) ", border:"2px solid var(--nude)"
-                }}>Login</button>
+                <button className='text-center my-4 col-3 col-sm-8 col-md-6 rounded-2  text-light ' style={{
+                    backgroundColor:"var(--darkpurple) ", border:"2px solid var(--nude)"}}
+                    type='submit' onSubmit={Handlesubmit}>Login</button>
                 <span className='text-danger'></span>
             </div>
-   </div>
+   </form>
    </>
   )
 }
