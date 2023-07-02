20d200affaea3400e1f03b96d178f16c05b1ad8a
malla sagar <mallasagar0101@gmail.com>
2023-07-02 11:41:25 +0545

    manage admin dashboard

--- src/admin/component/sidebar/sidebar.js
+++ src/admin/component/sidebar/sidebar.js
@@ -1,66 +1,71 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import {BiMessageAlt} from 'react-icons/bi'
+import {AiFillSetting,AiOutlineDashboard}from 'react-icons/ai'
+import{FiPackage} from 'react-icons/fi'
+import{GrGallery} from 'react-icons/gr'
+import {CiLogout} from 'react-icons/ci'
+import{BsEmojiHeartEyes} from 'react-icons/bs'
+import{MdOutlineDevicesOther} from 'react-icons/md'
+
+
 
 function Adminsidebar() {
   return (
    <>
- <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+ <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion py-3" id="accordionSidebar">
 
-    <NavLink>
-        <div class="sidebar-brand-icon rotate-n-15">
-         
-        </div>
-        <div class="sidebar-brand-text mx-3 ">Beauty in You </div>
-    </NavLink>
 
-    <hr class="sidebar-divider my-0"/>
+    <div class="sidebar-brand-text mx-auto fs-4 text-warning ">Beauty in You </div>
+    
 
-    <li class="nav-item">
+    <hr class="sidebar-divider bg-light mb-2"/>
+
+    <li class="nav-item my-2 ">
         <NavLink class="nav-link" to="/admin">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <AiOutlineDashboard className='mx-2' size={'18px'}></AiOutlineDashboard>
             <span>Dashboard</span></NavLink>
     </li>
 
-    <hr class="sidebar-divider"/>
   
-    <li class="nav-item">
+    <li class="nav-item  my-2 ">
         <NavLink class="nav-link" to="/admin/enquiry">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
+       <BiMessageAlt className='mx-2' size={'18px'}></BiMessageAlt>
             <span>Enquiry</span></NavLink>
     </li>
    
 
-    <li class="nav-item">
+    <li class="nav-item  my-2 ">
         <NavLink class="nav-link" to="/admin/service">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <AiFillSetting className='mx-2' size={'18px'}></AiFillSetting>
             <span>Service</span></NavLink>
     </li>
-    <li class="nav-item">
+    <li class="nav-item  my-2 ">
         <NavLink class="nav-link" to="/admin/package">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <FiPackage className='mx-2' size={'18px'}></FiPackage>
             <span>Packages</span></NavLink>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item  my-2 ">
         <NavLink class="nav-link" to="/admin/gallery">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
+        <GrGallery  className='mx-2' size={'18px'}></GrGallery>
             <span>Gallery</span></NavLink>
     </li>
-    <li class="nav-item">
+    <li class="nav-item  my-2 ">
         <NavLink class="nav-link" to="/admin/review">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Review</span></NavLink>
+            <BsEmojiHeartEyes className='mx-2' size={'18px'}></BsEmojiHeartEyes>
+            <span >Review</span></NavLink>
     </li>
-    <li class="nav-item">
+    <li class="nav-item  my-2 ">
         <NavLink class="nav-link" to="/admin/other">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <MdOutlineDevicesOther className='mx-2' size={'18px'}></MdOutlineDevicesOther>
             <span>Others</span></NavLink>
     </li>
    
-    <hr class="sidebar-divider"/>
-    <li class="nav-item">
+    <hr class="sidebar-divider bg-light"/>
+    <li class="nav-item ">
         <NavLink class="nav-link" to="/logout" >
-            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <CiLogout className='mx-2' size={'18px'}></CiLogout>
             <span>Logout</span></NavLink>
     </li>
 
